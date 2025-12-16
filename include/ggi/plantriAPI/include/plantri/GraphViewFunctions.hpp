ed81223e6084cffbac10fac7e15c169364f33f1d
dochynes <stas.dochinec@seznam.cz>
2025-12-16 12:02:41 +0100

    add Boost graph functions for GraphView

--- include/ggi/plantriAPI/include/plantri/GraphViewFunctions.hpp
+++ include/ggi/plantriAPI/include/plantri/GraphViewFunctions.hpp
@@ -37,6 +37,44 @@ inline std::pair<Traits::adjacency_iterator,Traits::adjacency_iterator> adjacenc
     return std::make_pair(Traits::adjacency_iterator(&g,v,first,deg),Traits::adjacency_iterator(&g,v,nullptr,0));
 };
 
+//incidence graph
+inline auto out_edges(Traits::vertex_descriptor u, const GraphView& g)
+{
+    if(u > 0 || u>=g.nv || u==g.missing_vertex)
+    {
+        return std::make_pair(Traits::out_edge_iterator(&g,u,nullptr,0),Traits::out_edge_iterator(&g,u,nullptr,0));
+    }
+
+    int deg = g.degree[u];
+    EDGE* first;
+    if(deg>0)
+        first = g.firstedge[u];
+    else
+        first = nullptr;
+
+    return std::make_pair(Traits::out_edge_iterator(&g,u,first,deg),Traits::out_edge_iterator(&g,u,nullptr,0));
+
+
+};
+
+
+inline Traits::degree_size_type out_degree(Traits::vertex_descriptor u,const GraphView& g)
+{
+    if (u < 0 || u >= g.nv || u == g.missing_vertex) 
+        return 0;
+    return g.degree[u];
+}
+
+
+inline auto vertices(const GraphView& g)
+{
+    return std::make_pair(Traits::vertex_iterator(&g,0),Traits::vertex_iterator(&g,g.nv));
+}
+
+inline auto num_vertices(const GraphView& g)
+{
+    return g.nv - (g.missing_vertex >= 0 ? 1 : 0);
+}
 
 
 
