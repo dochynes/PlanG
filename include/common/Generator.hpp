76f363df2a6dbf891607fa3ade4f5d04e0792a5d
dochynes <stas.dochinec@seznam.cz>
2026-01-21 03:31:19 +0100

    add Generator

---
+++ include/common/Generator.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "Output.hpp"
+
+
+// T = geng::Backend nebo plantri::Backend
+
+template<typename Backend>
+struct Generator
+{
+    using GraphView = typename Backend::ViewType;
+
+    static int run(int argc, char** argv)
+    {
+        return Backend::run(argc, argv);
+    }
+
+    template <typename Func>
+    static void setPrune(Func&& f) 
+    {
+        Backend::setPrune(std::forward<Func>(f));
+    }
+
+    template <typename Func>
+    static void setPreprune(Func&& f) 
+    {
+        Backend::setPreprune(std::forward<Func>(f));
+    }
+
+    template <typename Func>
+    static void setOutproc(Func&& f) 
+    {
+        Backend::setOutproc(std::forward<Func>(f));
+    }
+
+
+};
