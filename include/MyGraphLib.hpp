85083f2cc061dceaeb490e60f01212fa069f11da
dochynes <stas.dochinec@seznam.cz>
2026-02-03 18:48:16 +0100

    add MyGraphLib.hpp as a unified library header

---
+++ include/MyGraphLib.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "common/Generator.hpp"
+#include "common/Output.hpp"
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/sequential_vertex_coloring.hpp>
+#include <boost/property_map/property_map.hpp> 
+#include <boost/range/iterator_range.hpp>
+
+#include "geng/GengAPI.hpp"
+#include "geng/GraphViewBoost.hpp" 
+#include "geng/GraphViewFunctions.hpp"
+
+#include "plantri/PlantriAPI.hpp"
+#include "plantri/GraphViewBoost.hpp" 
+#include "plantri/GraphViewFunctions.hpp"
