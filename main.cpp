73b5f962487c0eea4fb4b0697a2d958e4635714f
dochynes <stas.dochinec@seznam.cz>
2026-05-24 22:39:04 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,17 @@
 #include "MyGraphLib.hpp"
-#include <iostream>
-// STEP 1: Select a backend.
-// Uncomment the one you wish to use. The rest of the code remains unchanged.
 
-// A) General graph generation (nauty/geng)
 using App = Generator<geng::Backend>;
 
-// B) Planar graph generation (plantri)
-// using App = Generator<plantri::Backend>;
-
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-    std::cout << "API Example";
-    // STEP 2: Configure generator parameters
-    App::setVertices(11);
-    App::setTriangleFree();
+    App app;
 
-    // (optional): Define the output
-    // Here we use 'operator<<', which is implemented for both backends
-    App::setOutproc([](Output& out, const App::GraphView& g) {   // We can use 'auto' here thanks to type deduction.
-        out << g; 
-    });
+    // Triangle-free generation uses the spaextend branch inside geng.
+    app.setVertices(4);
+    app.setTriangleFree();
+    app.setRootedVertices(1);
+    //app.setCanonicalLabeling();
 
-    return App::run();
+
+    return app.run();
 }
